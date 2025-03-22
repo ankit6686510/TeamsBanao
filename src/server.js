bf3a13700128de8423b729c29282fec6f221817a
Ankit Jha <ankit6686510@gmail.com>
2025-03-22 20:24:47 +0530

    exploring the get post and delete routes

--- src/server.js
+++ src/server.js
@@ -1,21 +1,25 @@
-const express = require('express');
+const express = require("express");
 
-const app =  express();
+const app = express();
 
-app.use("/",(req,res) => {
-    res.send("namaste from dashboard");
-
+//this will method only handle get call to /user
+app.get("/user",(req,res)=>{
+    res.send({firstname:"ankit" , secondname: "jha"});
 
 });
-
-app.use("/hello",(req,res) =>{
-    res.send("hello hello hello!!")
+app.post("/user",(req,res)=>{
+    res.send("data successfully saved to db");
+});
+app.delete("/user",(req,res)=>{
+    res.send("user is deleted successfully");
 })
 
-app.use("/test",(req,res) =>{
-    res.send("hello from server")
+//this will match all the http method api call to /test
+app.use("/test", (req, res) => {
+  res.send("hello from server");
 });
 
-app.listen(777,() =>{
-    console.log("server is successfully listening on port 777")
+
+app.listen(777, () => {
+  console.log("server is successfully listening on port 777");
 });
